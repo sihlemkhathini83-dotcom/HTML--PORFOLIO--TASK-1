1cc7711abbed78c43add46047e85bcac94321e81
kelvi <kelvi@SIHLEKELVIN>
2026-05-19 23:06:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTML__PORFOLIO__TASK_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
